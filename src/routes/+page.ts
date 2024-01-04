b150282b7b9687f70fe58b14fc92925803fe0330
broemp <derbrommi@live.de>
2024-01-04 01:57:49 +0100

    Remove trailing /

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,7 @@ import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch }) => {
 
-  const res = await fetch(PUBLIC_BACKEND_URL + '/cards/?page_id=1&page_size=10', {
+  const res = await fetch(PUBLIC_BACKEND_URL + '/cards?page_id=1&page_size=10', {
     method: "GET",
     mode: "cors"
   });
